5b380a4c0daa417a2df1dfa42f1f6ca5d7c858b3
Andrew Wang <57112250+AndrewxWang@users.noreply.github.com>
2020-07-10 04:04:02 -0400

    Update MathQuiz.py
    You can now pick how many questions and the highest number that can be generated before the tkinter window opens

--- MathQuiz.py
+++ MathQuiz.py
@@ -1,8 +1,38 @@
 from tkinter import *
 import random
+import time
 
+q_num = 0
+r_num = 0
 root = Tk()
+root.withdraw()
+
+start_game = False
+while (not start_game):
+    q_num = input("Enter the number of questions: ")
+    time.sleep(1)
+    if q_num.isdigit() and int(q_num) > 0:
+        q_num = int(q_num)
+        while (not start_game):
+            r_num = input("Enter highest randomly generated number: ")
+            time.sleep(1)
+            if r_num.isdigit() and int(r_num) > 0:
+                print("Starting game...")
+                time.sleep(1)
+                print("Click the \"new question\" button to start!")
+                time.sleep(1)
+                root.lift()
+                root.deiconify()
+                r_num = int(r_num)
+                start_game = True
+                break
+            else:
+                print("That is not a valid input!")
+    else:
+        print("That is not a valid input!")
+
 root.geometry("675x400")
+root.focus_set()
 ans =IntVar()
 questions = 1
 choice = -1
@@ -11,6 +41,7 @@ equation = []
 rand = []
 submit_button = Button
 startt = Button
+#a,b,c,d choices
 ansA = Radiobutton
 ansB = Radiobutton
 ansC = Radiobutton
@@ -18,8 +49,8 @@ ansD = Radiobutton
 
 def createEq(signs):
     eq=[]
-    a= random.randint(1,10)
-    b= random.randint(1,10)
+    a= random.randint(1,r_num)
+    b= random.randint(1,r_num)
     eq.append(a)
     eq.append(b)
     sign= random.choice(signs)
@@ -71,9 +102,9 @@ def buttonPressed():
     print("Correct Answer: " + str(answ))
     if rand[choice] == answ:
         score+=1
-        canvas.create_text(150,100, text="Correct! Score: " + str(score) + "/10")
+        canvas.create_text(150,100, text="Correct! Score: " + str(score) + "/" + str(q_num))
     else:
-        canvas.create_text(150,100,text="Wrong! " + str(score) + "/10")
+        canvas.create_text(150,100,text="Wrong! Score: " + str(score) + "/" + str(q_num))
     submit_button["state"] = DISABLED
     startt["state"] = NORMAL
     ansA["state"] = DISABLED
@@ -104,18 +135,18 @@ def answD():
     submit_button["state"] = NORMAL
 
 def createGame():
+    canvas.delete("all")
     signs=["+","-","*","/"]
     global submit_button, startt
     global equation, questions, ans, rand
     global score
     global ansA, ansB, ansC, ansD
-    canvas.delete("all")
     equation = createEq(signs)
     answers = createAns(equation)
     rand = randomAns(answers)
     ans.set(0)
     #BUTTONS
-    if questions <= 10:
+    if questions <= q_num:
         print("Question: " + str(questions))
         eq = Button(root, font='Helvetica 10 bold',text=" Question " + str(questions) + ": " + str(equation[0]) + " " + str(equation[2]) + " " + str(equation[1]) + " = ?   ",borderwidth=0)
         ansA=Radiobutton(root,text="A. " + str(rand[0]), variable=ans, value=1,command=answA)
@@ -141,12 +172,13 @@ def createGame():
         startt["state"] = DISABLED
         submit_button["state"] = DISABLED
     else:
-        Score_label = Label(root,font="Arial 10 bold",text="Quiz complete! Final Score: " + str(score) + "/10")
+        Score_label = Label(root,font="Arial 10 bold",text="Quiz complete! Final Score: " + str(score) + "/" + str(q_num))
         Score_label.place(x=250,y=200)
         startt["state"] = DISABLED
 ##    print(rand)
 ##    print(equation)
 ##    print(questions)
+
 canvas = Canvas(root, bg="gainsboro", height=250, width=300)
 canvas.place(x=200, y=100)
 startt = Button(root, text="new question", command = createGame)
